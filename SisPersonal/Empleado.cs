11639d3220d220f4ff47cafbb5421d0a5cba29d0
maria belen <tracannab@gmail.com>
2025-06-17 20:02:27 -0300

    modifique en carpeta sispersonal - agregue el adicional sueldo

---
+++ SisPersonal/Empleado.cs
@@ -0,0 +1,111 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace Administracion
+{
+    // los enum ya tienen un numero enmascarado auxiliar = 1 , si no le pongo numero comienza en cero, como los arreglos
+    public enum Cargo
+    {
+        Auxiliar = 1,
+        Administrativo = 2,
+        Ingeniero = 3,
+        Especialista = 4,
+        Investigador = 5
+    }
+    public class Empleado
+    {
+        private string nombre; // siempre los campos son privados 
+        private string apellido;
+        private DateTime fecha_nacimiento;
+        private char estado_civil;
+        private DateTime fecha_ingreso;
+        private double sueldo_basico;
+        private Cargo cargo;
+
+        // constructor vacio (situarme seleccionando todos los datos ( ctrl.) y me genera el constructor)
+        //defino los parametros
+
+
+        public Empleado(string nombre, string apellido, DateTime fecha_nacimiento, char estado_civil, DateTime fecha_ingreso, double sueldo_basico, Cargo cargos)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.fecha_nacimiento = fecha_nacimiento;
+            this.estado_civil = estado_civil;
+            this.fecha_ingreso = fecha_ingreso;
+            this.sueldo_basico = sueldo_basico;
+            this.cargo = cargo; 
+        }// me genera todos los parametros
+         // nombres de los metodos publicos empiezan por mayuscula
+        public int Antiguedad() // metodo equivalente a una funcion
+        {
+            //fecha actual - fecha ingreso
+            DateTime fecha_actual = DateTime.Now; // fecha actual con una propiedad get
+            int antiguedad = fecha_actual.Year - fecha_ingreso.Year;
+
+            if (fecha_actual.Month < fecha_ingreso.Month || fecha_actual.Month == fecha_ingreso.Month && fecha_actual.Day < fecha_ingreso.Day)
+            {
+                antiguedad--;// me reste un año
+            }
+            return antiguedad;
+
+        }
+
+        public int Edad()//metodo equivalente a una funcion
+        {
+            DateTime fecha_actual = DateTime.Now;// fecha actual con una propiedad get
+            int edad = fecha_actual.Year - fecha_nacimiento.Year;
+            if (fecha_actual.Month < fecha_nacimiento.Month || fecha_actual.Month == fecha_nacimiento.Month && fecha_actual.Day < fecha_actual.Day)
+            {
+                edad--;
+            }
+            return edad;
+        }
+        /*      public int AñosJubilacion()
+                {
+                    int edad = Edad();
+                    int falta_para_jubilarme = 65 - edad;
+
+                    return falta_para_jubilarme;
+                }
+        */
+
+        public int AñosJubilacion()
+        {
+            return 65 - Edad(); // llamo a la funcion edad ya creada.
+        }
+
+        public double Salario()
+        {
+            double adicional = 0;
+            if (Antiguedad() < 20)
+            {
+                adicional = sueldo_basico * Antiguedad() / 100;
+            }
+            else
+            {
+                adicional = sueldo_basico * 0.25;
+            }
+            if (cargo == Cargo.Ingeniero || cargo == Cargo.Especialista)
+            {
+                adicional = adicional * 0.50;
+            }
+            if (estado_civil == 'c')
+            {
+                adicional = adicional + 150000;
+            }
+
+            return sueldo_basico + adicional;
+        }
+    }
+
+
+        
+
+    }
+ 
--- SisPersonal/Program.cs
+++ SisPersonal/Program.cs
@@ -16,4 +16,36 @@ Sueldo Básico (double),
 g. Cargo (cargos)  
 Nota: Cargo es una variable de tipo “cargos” que es enumeración 
 (enum) que ud debería definir con los siguientes elementos: Auxiliar
+Administrativo;  Ingeniero; Especialista; Investigador. (investigue el uso 
+de  enum en C# para realizar esto) 
+2. Cree los métodos necesarios para poder obtener los datos que se detallan a 
+continuación: 
+a. Calcular lo siguiente: 
+i. 
+La antigüedad del empleado en la empresa.  
+ii. 
+iii. 
+La edad del empleado, 
+La cantidad de años que le falta al empleado para poder 
+jubilarse, considerando que la edad de jubilación es de 65.  
+b. Calcular el salario, de acuerdo a la fórmula: Salario = Sueldo Básico + 
+Adicional. Para ello el Adicional contempla la antigüedad en años, el 
+cargo y si es casado: 
+i) 1 % del sueldo básico por cada año de antigüedad hasta los 
+20 años, a partir de este, el porcentaje se fija en 25%. 
+ii) Si el cargo es Ingeniero o Especialista, el Adicional se 
+incrementa en un 50%. 
+iii)   Si es casado al Adicional se le aumenta $150.000.  
+Por ejemplo, si la antigüedad es de 15 años y el Sueldo Básico = 
+$650.000, el Adicional es 65.0000 * 0.15 = 97.500. Si además el 
+cargo es Ingeniero o Especialista, se incrementa el Adicional en 
+un 50%. Esto es: 97.500* 1.50 = 146.250. Si a su vez es casado 
+el Adicional será: 146.250+ 150.000= 296.250 
+c. Cargue los datos para 3 empleados en un arreglo de tipo empleados. 
+d. Obtener el Monto Total de lo que se paga en concepto de Salarios. 
+e. Muestre por pantalla los datos del empleado que esté más próximo a 
+jubilarse (incluyendo los datos del apartado 2.a y el salario 
+correspondiente.  
+NOTA: Los puntos a y b deben estar definidos dentro de la clase como 
+propiedades o métodos según corresponda  
 */
